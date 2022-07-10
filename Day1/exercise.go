7da96611ad927ed1ebd738044b3f0888a33d3d69
Laksh Sadhwani <laksh.sadhwani@razorpay.com>
2022-07-10 23:21:26 +0530

    seperated sections for each question of exercise

--- Day1/exercise.go
+++ Day1/exercise.go
@@ -1,148 +1,151 @@
- // question 1
-//package main
-//
-//import (
-//	"encoding/json"
-//	"fmt"
-//	"log"
-//)
-//
-//type matrix struct {
-//	rows int
-//	columns int
-//	ele [3][3] int
-//
-//}
-//
-//func (r * matrix) getRows() int{
-//	return r.rows
-//}
-//func (r * matrix) getColumn() int{
-//	return r.columns
-//}
-//func (r * matrix) setEle(){
-//	row:= r.rows
-//	col:=r.columns
-//	arr:=r.ele // declared an empty 2d array
-//	for i:=0; i<row; i++{
-//		for j:=0 ; j < col; j++{
-//			fmt.Print(arr[i][j], " ") // assigned values to it
-//		}
-//		fmt.Print("\n")
-//	}
-//}
-//func (r *matrix) addMatrix(){
-//	row:= r.rows
-//	col:=r.columns
-//	arr:=r.ele
-//	var mat = [3][3]int{{1,1,1},{1,1,1},{1,1,1}}
-//	for i:=0; i<row; i++{
-//		for j:=0 ; j < col; j++{
-//			arr[i][j] = arr[i][j] + mat[i][j]
-//		}
-//	}
-//	tempFunc := matrix{3,3,arr}
-//	tempFunc.setEle()
-//}
-//// unable to print array as json
-//func (r * matrix) printJson(){
-//	arr:=r.ele
-//	convArr, err:= json.Marshal(arr)
-//	if err == nil{
-//		log.Fatal(err)
-//	}
-//	fmt.Println(convArr)
-//
-//}
-//func main()  {
-//	fmt.Println("Five Functions")
-//	var one= [3][3] int {{1,2,3},{4,5,6},{7,8,9}}
-//	var emptyArr= [3][3] int{}
-//	firstFunc := matrix{3,3, emptyArr}
-//	secondFunc := matrix{3,3, one}
-//	fmt.Println(firstFunc.rows)
-//	fmt.Println(firstFunc.columns)
-//	secondFunc.setEle()
-//	fmt.Print("\n")
-//	secondFunc.addMatrix()
-//	secondFunc.printJson()
-//}
+// Question 1
+package main
 
-// question 2
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
- //package main
- //
- //import (
-	// "fmt"
-	// "io"
-	// "os"
- //)
- //
- //type TreeNode struct {
-	// left  *TreeNode
-	// right *TreeNode
-	// data  int
- //}
- //
- //type Tree struct {
-	// root *TreeNode
- //}
- //
- //func (t *Tree) insert(data int) *Tree {
-	// if t.root == nil {
-	//	 t.root = &TreeNode{data: data, left: nil, right: nil}
-	// } else {
-	//	 t.root.insert(data)
-	// }
-	// return t
- //}
- //
- //func (n *TreeNode) insert(data int) {
-	// if n == nil {
-	//	 return
-	// } else if data <= n.data {
-	//	 if n.left == nil {
-	//		 n.left = &TreeNode{data: data, left: nil, right: nil}
-	//	 } else {
-	//		 n.left.insert(data)
-	//	 }
-	// } else {
-	//	 if n.right == nil {
-	//		 n.right = &TreeNode{data: data, left: nil, right: nil}
-	//	 } else {
-	//		 n.right.insert(data)
-	//	 }
-	// }
- //}
- //
- //func print(w io.Writer, node *TreeNode, ns int, ch rune) {
-	// if node == nil {
-	//	 return
-	// }
- //
-	// for i := 0; i < ns; i++ {
-	//	 fmt.Fprint(w, " ")
-	// }
-	// fmt.Fprintf(w, "%c:%v\n", ch, node.data)
-	// print(w, node.left, ns+2, 'L')
-	// print(w, node.right, ns+2, 'R')
- //}
- //
- //func main() {
-	// tree := &Tree{}
-	// tree.insert(100).
-	//	 insert(-20).
-	//	 insert(-9).
-	//	 insert(-5).
-	//	 insert(-1).
-	//	 insert(44).
-	//	 insert(6).
-	//	 insert(42).
-	//	 insert(35).
-	//	 insert(5).
-	// print(os.Stdout, tree.root, 0, 'M')
- //}
+type matrix struct {
+	rows int
+	columns int
+	ele [3][3] int
 
+}
+
+func (r * matrix) getRows() int{
+	return r.rows
+}
+func (r * matrix) getColumn() int{
+	return r.columns
+}
+func (r * matrix) setEle(){
+	row:= r.rows
+	col:=r.columns
+	arr:=r.ele // declared an empty 2d array
+	for i:=0; i<row; i++{
+		for j:=0 ; j < col; j++{
+			fmt.Print(arr[i][j], " ") // assigned values to it
+		}
+		fmt.Print("\n")
+	}
+}
+func (r *matrix) addMatrix(){
+	row:= r.rows
+	col:=r.columns
+	arr:=r.ele
+	var mat = [3][3]int{{1,1,1},{1,1,1},{1,1,1}}
+	for i:=0; i<row; i++{
+		for j:=0 ; j < col; j++{
+			arr[i][j] = arr[i][j] + mat[i][j]
+		}
+	}
+	tempFunc := matrix{3,3,arr}
+	tempFunc.setEle()
+}
+// Unable to print array as json
+func (r * matrix) printJson(){
+	arr:=r.ele
+	convArr, err:= json.Marshal(arr)
+	if err == nil{
+		log.Fatal(err)
+	}
+	fmt.Println(convArr)
+
+}
+func main()  {
+	fmt.Println("Five Functions")
+	var one= [3][3] int {{1,2,3},{4,5,6},{7,8,9}}
+	var emptyArr= [3][3] int{}
+	firstFunc := matrix{3,3, emptyArr}
+	secondFunc := matrix{3,3, one}
+	fmt.Println(firstFunc.rows)
+	fmt.Println(firstFunc.columns)
+	secondFunc.setEle()
+	fmt.Print("\n")
+	secondFunc.addMatrix()
+	secondFunc.printJson()
+}
+// Question one end here
+
+// Question 2
+
+ package main
+
+ import (
+	"fmt"
+	"io"
+	"os"
+ )
+
+ type TreeNode struct {
+	left  *TreeNode
+	right *TreeNode
+	data  int
+ }
+
+ type Tree struct {
+	root *TreeNode
+ }
+
+ func (t *Tree) insert(data int) *Tree {
+	if t.root == nil {
+		 t.root = &TreeNode{data: data, left: nil, right: nil}
+	} else {
+		 t.root.insert(data)
+	}
+	return t
+ }
+
+ func (n *TreeNode) insert(data int) {
+	if n == nil {
+		 return
+	} else if data <= n.data {
+		 if n.left == nil {
+			 n.left = &TreeNode{data: data, left: nil, right: nil}
+		 } else {
+			 n.left.insert(data)
+		 }
+	} else {
+		 if n.right == nil {
+			 n.right = &TreeNode{data: data, left: nil, right: nil}
+		 } else {
+			 n.right.insert(data)
+		 }
+	}
+ }
+
+ func print(w io.Writer, node *TreeNode, ns int, ch rune) {
+	if node == nil {
+		 return
+	}
+
+	for i := 0; i < ns; i++ {
+		 fmt.Fprint(w, " ")
+	}
+	fmt.Fprintf(w, "%c:%v\n", ch, node.data)
+	print(w, node.left, ns+2, 'L')
+	print(w, node.right, ns+2, 'R')
+ }
+
+ func main() {
+	tree := &Tree{}
+	tree.insert(100).
+		 insert(-20).
+		 insert(-9).
+		 insert(-5).
+		 insert(-1).
+		 insert(44).
+		 insert(6).
+		 insert(42).
+		 insert(35).
+		 insert(5).
+	print(os.Stdout, tree.root, 0, 'M')
+ }
+// Question 2 ends here
+
+// Question 3
  package main
 
  import "fmt"
@@ -170,7 +173,7 @@
 	 calculate(fullTime)
 	 calculate(conTract)
 	 calculate(freeLancer)
-
-
  }
+
+ // Question 3 ends here
 
--- Day2/exercise.go
+++ Day2/exercise.go
@@ -1,72 +1,77 @@
-//package main
-//
-//import (
-//	"fmt"
-//	"strings"
-//	"sync"
-//)
-//func calculate(string2 string){
-//	fmt.Println(string2)
-//	for _,val := range string2{
-//		ch :=strings.Count(string2,string(val))
-//		fmt.Println(string(val), " - ", ch)
-//	}
-//}
-//
-//func process(i string, vr * sync.WaitGroup, ms *sync.Mutex) {
-//	ms.Lock()
-//	calculate(i)
-//	ms.Unlock()
-//	vr.Done()
-//}
-//func main(){
-//  fmt.Println("Question one")
-//	str := []string{"Hi", "hey", "laksh", "ball", "razorpay"}
-//	var ch sync.WaitGroup
-//	var ms sync.Mutex
-//	for _,bt := range str {
-//		ch.Add(1)
-//		go process(bt, &ch, &ms)
-//	}
-//	ch.Wait()
-//	fmt.Println("Printed count of alphabets in all the strings")
-//
-//
-//}
+// Question 1
+package main
 
-//package main
-//
-//import (
-//	"fmt"
-//	"math/rand"
-//	"sync"
-//	"time"
-//)
-//
-//func review(i int, sc * sync.WaitGroup, sum *int, mt *sync.Mutex)  {
-//	rand.Seed(time.Now().UnixNano())
-//	mt.Lock()
-//	time.Sleep(time.Millisecond * time.Duration(rand.Intn(1000)))
-//	pt := rand.Intn(10)
-//	*sum += pt
-//	fmt.Println("Student : " ,i, "Rating is", pt)
-//	mt.Unlock()
-//	sc.Done()
-//}
-//func main(){
-//	fmt.Println("Question 2")
-//	num:=200
-//	var sum int
-//	var sc sync.WaitGroup
-//	var mt sync.Mutex
-//	for i:=0 ; i < num ; i++{
-//		sc.Add(1)
-//		go review(i, &sc,&sum,&mt)
-//	}
-//	sc.Wait()
-//	fmt.Println("Average Rating : ", sum/200 )
-//}
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+)
+func calculate(string2 string){
+	fmt.Println(string2)
+	for _,val := range string2{
+		ch :=strings.Count(string2,string(val))
+		fmt.Println(string(val), " - ", ch)
+	}
+}
 
+func process(i string, vr * sync.WaitGroup, ms *sync.Mutex) {
+	ms.Lock()
+	calculate(i)
+	ms.Unlock()
+	vr.Done()
+}
+func main(){
+ fmt.Println("Question one")
+	str := []string{"Hi", "hey", "laksh", "ball", "razorpay"}
+	var ch sync.WaitGroup
+	var ms sync.Mutex
+	for _,bt := range str {
+		ch.Add(1)
+		go process(bt, &ch, &ms)
+	}
+	ch.Wait()
+	fmt.Println("Printed count of alphabets in all the strings")
+}
+// Question 1 ends here
+
+// Question 2
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func review(i int, sc * sync.WaitGroup, sum *int, mt *sync.Mutex)  {
+	rand.Seed(time.Now().UnixNano())
+	mt.Lock()
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(1000)))
+	pt := rand.Intn(10)
+	*sum += pt
+	fmt.Println("Student : " ,i, "Rating is", pt)
+	mt.Unlock()
+	sc.Done()
+}
+func main(){
+	fmt.Println("Question 2")
+	num:=200
+	var sum int
+	var sc sync.WaitGroup
+	var mt sync.Mutex
+	for i:=0 ; i < num ; i++{
+		sc.Add(1)
+		go review(i, &sc,&sum,&mt)
+	}
+	sc.Wait()
+	fmt.Println("Average Rating : ", sum/200 )
+}
+// Question 2 ends here
+
+// Question 3
 
 package main
 
@@ -106,6 +111,6 @@ func main(){
 		fmt.Println("Remaining Balance", bal)
 	}
 	fmt.Println("Final Balance", bal)
-
 // need to make change here as the updated balance equation should be placed in the critical section
 }
+// Question 3 ends here
